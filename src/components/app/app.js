edbd0204ca06cf4fce2163b6fe3d2035fc4b804a
ZerGuy <zergay94@gmail.com>
2019-03-19 00:33:33 +0300

    40

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -7,25 +7,44 @@ import ItemStatusFilter from '../item-status-filter';
 
 import './app.css';
 
-const App = () => {
+export default class App extends React.Component {
+  state = {
+    todoData: [
+      { label: 'Drink Coffee', important: false, id: 1 },
+      { label: 'Make Awesome App', important: true, id: 2 },
+      { label: 'Have a lunch', important: false, id: 3 }
+    ]
+  };
 
-  const todoData = [
-    { label: 'Drink Coffee', important: false, id: 1 },
-    { label: 'Make Awesome App', important: true, id: 2 },
-    { label: 'Have a lunch', important: false, id: 3 }
-  ];
+  deleteItem = (id) => {
+    this.setState(({todoData}) => {
+      const idx = todoData.findIndex(el => el.id === id);
+      const newArray = [
+        ...todoData.slice(0, idx), 
+        ...todoData.slice(idx + 1)
+      ];
 
-  return (
-    <div className="todo-app">
-      <AppHeader toDo={1} done={3} />
-      <div className="top-panel d-flex">
-        <SearchPanel />
-        <ItemStatusFilter />
+      return {
+        todoData: newArray
+      };
+    });
+  }
+
+  render() {
+    const {todoData} = this.state;
+
+    return (
+      <div className="todo-app">
+        <AppHeader toDo={1} done={3} />
+        <div className="top-panel d-flex">
+          <SearchPanel />
+          <ItemStatusFilter />
+        </div>
+  
+        <TodoList 
+          todos={todoData} 
+          onDeleted= {this.deleteItem} />
       </div>
-
-      <TodoList todos={todoData} />
-    </div>
-  );
+    );
+  }
 };
-
-export default App;
