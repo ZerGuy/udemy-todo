effe9ab8223f85957a7b558d5d005cfd4506881a
solenevEPAM <SOlenev@idbs.com>
2019-03-18 13:16:43 +0300

    30

--- src/index.js
+++ src/index.js
@@ -7,11 +7,17 @@ import SearchPanel from './components/search-panel'
 
 
 const App = () => {
+    const todoData = [
+        { label: 'Drink Coffee', important: false },
+        { label: 'Make React App', important: true },
+        { label: 'Have a lucnh', important: false },
+    ] ;
+
     return (
         <div>
             <AppHeader />
             <SearchPanel />
-            <TodoList />
+            <TodoList todos={todoData}/>
         </div>
     )
 };
